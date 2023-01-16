53430affaf734add5ddc5c15270ae0d1542ddc94
swanine <haohao1500@foxmail.com>
2023-01-16 21:29:22 +0800

    基础学习

--- MyPlayground.playground/Pages/02-数据类型.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/02-数据类型.xcplaygroundpage/Contents.swift
@@ -1,9 +1,11 @@
 //常量 使用前必须赋值一次
 
+// 字符串
 let message = "hello, swift!"
 
 //message = "s"
 
+// 整形
 let age: Int
 
 //age = 26
@@ -15,3 +17,7 @@ func getAge() -> Int {
 age = getAge()
 
 print(age)
+
+// double
+var PI = 3.14
+
